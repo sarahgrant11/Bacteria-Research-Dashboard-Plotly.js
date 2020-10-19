aa1147d1078f5517fbfe6afc18ee6d9669e1dabd
Sarah Grant <sarahgrant11@gmail.com>
2020-10-19 13:06:10 -0400

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,68 @@
-# plotly-challenge
+# Plot.ly Homework - Belly Button Biodiversity
+
+In this assignment, you will build an interactive dashboard to explore the Belly Button Biodiversity dataset, which catalogs the microbes that colonize human navels.
+
+The dataset reveals that a small handful of microbial species (also called operational taxonomic units, or OTUs, in the study) were present in more than 70% of people, while the rest were relatively rare.
+
+Step 1: Plotly
+Use the D3 library to read in samples.json.
+
+Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
+
+Use sample_values as the values for the bar chart.
+
+Use otu_ids as the labels for the bar chart.
+
+Use otu_labels as the hovertext for the chart.
+
+bar Chart
+
+Create a bubble chart that displays each sample.
+Use otu_ids for the x values.
+
+Use sample_values for the y values.
+
+Use sample_values for the marker size.
+
+Use otu_ids for the marker colors.
+
+Use otu_labels for the text values.
+
+Bubble Chart
+
+Display the sample metadata, i.e., an individual's demographic information.
+
+Display each key-value pair from the metadata JSON object somewhere on the page.
+
+hw
+
+Update all of the plots any time that a new sample is selected.
+Additionally, you are welcome to create any layout that you would like for your dashboard. An example dashboard is shown below:
+
+hw
+
+Advanced Challenge Assignment (Optional)
+The following task is advanced and therefore optional.
+
+Adapt the Gauge Chart from https://plot.ly/javascript/gauge-charts/ to plot the weekly washing frequency of the individual.
+
+You will need to modify the example gauge code to account for values ranging from 0 through 9.
+
+Update the chart whenever a new sample is selected.
+
+Weekly Washing Frequency Gauge
+
+Deployment
+Deploy your app to a free static page hosting service, such as GitHub Pages. Submit the links to your deployment and your GitHub repo.
+
+Ensure your repository has regular commits (i.e. 20+ commits) and a thorough README.md file
+
+Hints
+Use console.log inside of your JavaScript code to see what your data looks like at each step.
+
+Refer to the Plotly.js documentation when building the plots.
+
+About the Data
+Hulcr, J. et al.(2012) A Jungle in There: Bacteria in Belly Buttons are Highly Diverse, but Predictable. Retrieved from: http://robdunnlab.com/projects/belly-button-biodiversity/results-and-data/
+
+© 2019 Trilogy Education Services
---
+++ static/js/app.js
@@ -0,0 +1,161 @@
+// Set the URL to import the data json
+const url = "https://raw.githubusercontent.com/sarahgrant11/plotly-challenge/master/data/samples.json";
+
+// Fetch the JSON data and console log it
+// d3.json(url).then((data) => {
+//     var sampleData = data;
+//     console.log(sampleData);
+//     var metadata = data.metadata;
+//     console.log(metadata);
+//     var names = data.names;
+//     console.log(names);
+//     var samples = data.samples;
+//     console.log(samples);
+// });
+
+// Initialize page with Test Subject ID dropdown options and display data for first ID
+function init() {
+    // Grab a reference to the dropdown select element
+    var dropdown = d3.select("#selDataset");
+    //pull all IDs from names
+    d3.json(url).then(function (IDs) {
+        var IDs = IDs.names;
+        IDs.forEach((id) => {
+          dropdown
+            .append("option")
+            .text(id)
+            .property("value", id);
+        });
+    //set the initial ID to the first name
+    var firstID = IDs[0];
+    //build the plots with the firstID
+    buildDemos(firstID);
+    buildPlots(firstID);
+    });
+};
+
+//Populate the Demographic Info section based on selected ID
+function buildDemos(ID) {
+    d3.json(url).then(function (data) {
+    var metadata = data.metadata;
+    //filter to get metadata for passed ID
+    var filteredDemo = metadata.filter(metadataID => metadataID.id == ID)[0];
+    //clear any previous metadata
+    d3.select('#sample-metadata').html('');
+    //add each key value pair from metaData
+        Object.entries(filteredDemo).forEach(([key, value]) => {
+            d3.select('#sample-metadata')
+            .append('p').text(`${key}: ${value}`);
+        });
+    });
+};
+
+function buildPlots(ID) {
+    d3.json(url).then(function(plotData) {
+        //filter samples data to ID for plotting
+        var samplePlot = plotData.samples.filter(plotID => plotID.id == ID)[0];
+        //console.log(samplePlot);
+        //slice top 10 of each samples data: otu_ids, otu_labels, and sample_values
+        //map otu_ids to string with OTU label
+        var slice_otu_ids = samplePlot.otu_ids.slice(0, 10).map(id => "OTU "+id.toString());
+        var slice_otu_labels = samplePlot.otu_labels.slice(0, 10);
+        var slice_sample_values = samplePlot.sample_values.slice(0, 10);
+        
+        //BAR CHART plot
+        var traceBar = {
+            type: 'bar',
+            x: slice_sample_values.reverse(),
+            y: slice_otu_ids.reverse(),
+            text: slice_otu_labels.reverse(),
+            marker: {
+                color: '#1978B5',
+              },
+            orientation: 'h'
+        };
+
+        var barData = [traceBar];
+
+        var barLayout = {
+            title: 'Top 10 Microbes (OTUs) Found',
+            showlegend: false,
+            width: 600,
+            height: 400
+        };
+
+        Plotly.newPlot('bar', barData, barLayout);
+        //console.log(barData);
+
+        //BUBBLE CHART
+        var otu_ids = samplePlot.otu_ids.slice(0, 10);
+        var traceBubble = {
+            type: 'bubble',
+            x: otu_ids,
+            y: slice_sample_values,
+            mode: 'markers',
+            marker: {
+                color: otu_ids,
+                colorscale: 'Viridis',
+                size: slice_sample_values
+              },
+            text: slice_otu_labels
+        };
+
+        var bubbleData = [traceBubble];
+
+        var bubbleLayout = {
+            title: "Top 10 Microbes Bubble Chart",
+            x: "OTU ID",
+            pointStyle: "circle"
+        }
+
+        Plotly.newPlot('bubble', bubbleData, bubbleLayout);
+
+        //GAUGE CHART
+        //find the wash frequency variable
+        var metadata = plotData.metadata.filter(plotID => plotID.id == ID)[0];
+        var washFreq = metadata.wfreq;
+        console.log(washFreq);
+
+        var traceGauge = {
+            domain: { x: [0, 1], y: [0, 1] },
+            value: washFreq,
+            title: { text: 'Bellybutton WashFrequency'},
+            type: "indicator",
+            mode: "gauge+number",
+            gauge: {
+                axis: { range: [null, 9] },
+                steps: [
+                    { range: [0, washFreq], color: '#1978B5' }
+                ],
+                    threshold: {
+                    line: { color: "red", width: 4 },
+                    thickness: 0.75,
+                        value: washFreq
+                }
+            }
+        };
+
+        var gaugeData = [traceGauge];
+
+        var gaugeLayout = {
+            width: 500, height: 400, margin: { t: 0, b: 0 }
+        };
+
+        Plotly.newPlot('gauge', gaugeData, gaugeLayout);
+        
+    });
+};
+
+//on ID dropwdown change, rebuild plots
+function optionChanged(newID) {
+    //     // Fetch new data each time a new sample is selected
+    buildDemos(newID);
+    buildPlots(newID);
+  };
+
+//d3.select('#selDataset').on('change', {
+  //buildDemos(this.value)
+//})
+
+//run init to start
+init();
